c51f26cc6232f8179b43f9a0147fff0c9eac11b6
Adrian Garrido Beas <91611871+Ercamarero@users.noreply.github.com>
2024-05-02 13:47:39 +0200

    Update test_timeseriesfinanceclient.py

--- tests/finance/test_timeseriesfinanceclient.py
+++ tests/finance/test_timeseriesfinanceclient.py
@@ -26,7 +26,7 @@ def test_constructor_failure_invalid_api_key():
 @patch('request.get')
 def test_constructor_unsuccessful_request(api_key_str, mocked_requests):
     with pytest.raises(FinanceClientAPIError):
-        TimeSeriesFinanceClient("IBM", api_key_str)
+        TimeSeriesFinanceClient("IBM")
 
 
 def test_constructor_invalid_data(api_key_str, mock_build_data):
